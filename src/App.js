d18361e1c402c3e5ceef2e9a283fd87011766fdf
Michel Reyes <mreyesh85+1@gmail.com>
2020-04-01 22:46:52 -0400

    Public and Priivate Routes
    Added 2 HOCs because React doesn’t provide a way to check the authenticated state.
    
    1. wrap a <Route>
    2. pass props from the router to the <Route>
    3. render the component depending on the authenticated state, and
    4. redirect the user to a specified route if the condition is not met

--- src/App.js
+++ src/App.js
@@ -7,6 +7,33 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import { auth } from './services/firebase';
 
+//---------------------------------------------------------------
+
+function PrivateRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authenticated === true
+        ? <Component {...props} />
+        : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />}
+    />
+  )
+}
+
+//---------------------------------------------------------------
+
+function PublicRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authenticated === false
+        ? <Component {...props} />
+        : <Redirect to='/chat' />}
+    />
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
